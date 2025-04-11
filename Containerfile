a3099fdb9a666032915d340e4fb73b088ea049b1
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2025-04-11 22:26:47 +0100

    Get Intune to install...I think

--- Containerfile
+++ Containerfile
@@ -3,16 +3,9 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/bazzite:stable
-
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
+# FROM ghcr.io/ublue-os/aurora-dx:lts
+# Until Microsoft release RHEL 10 packages for Intune there is just no way the CentOS 10 image is going to work :()
+FROM ghcr.io/ublue-os/aurora-dx:41
 
 ### MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,13 @@
 
 set -ouex pipefail
 
-### Install packages
+### Install packagesq
+curl -L -o /etc/yum.repos.d/microsoft-rhel9.0-prod.repo https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod/config.repo
+curl -o /etc/yum.repos.d/microsoft-edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Undo the work of good open source maintainers https://github.com/ublue-os/bluefin-lts/pull/425
+# Required because the Microsoft packages are an absolute joke
+rm -rf /opt && mkdir /opt
+dnf install -y intune-portal microsoft-edge-stable
+mv /opt/* /var/opt/
+rm -rf /opt && ln -s /var/opt /opt
